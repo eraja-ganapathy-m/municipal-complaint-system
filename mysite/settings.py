fd353fab34da2ab4a8f8889573c41f1dd84086e4
ERAJA GANAPATHY M <70874759+eraja-ganapathy-m@users.noreply.github.com>
2021-05-22 09:31:07 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=( os.path.join(BASE_DIR, 'assets'),
-)
-
+STATICFILES_DIRS=( os.path.join(BASE_DIR, 'assets')
+                  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
